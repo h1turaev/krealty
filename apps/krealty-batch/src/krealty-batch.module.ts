fb3cdc9f18bc36c8e064a8cad3b50f9ef6684f05
Ray <tuhucu97@gmail.com>
2025-10-15 07:01:25 +0900

    feat: integration env variables

--- apps/krealty-batch/src/krealty-batch.module.ts
+++ apps/krealty-batch/src/krealty-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { KrealtyBatchController } from './krealty-batch.controller';
 import { KrealtyBatchService } from './krealty-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [KrealtyBatchController],
   providers: [KrealtyBatchService],
 })
--- apps/krealty-batch/src/krealty-batch.service.ts
+++ apps/krealty-batch/src/krealty-batch.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class KrealtyBatchService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Welcome to KRealty Batch!';
   }
 }
