1f5655344b310ed64612a9dce2ab1ec26c98b194
humoyunturaev <h@MacBook-Pro-h.local>
2025-11-16 02:16:06 +0900

    feat: register NotificationModule in ComponentsModule

--- apps/krealty-api/src/components/components.module.ts
+++ apps/krealty-api/src/components/components.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-import { MemberModule } from './member/member.module';
-import { PropertyModule } from './property/property.module';
 import { AuthModule } from './auth/auth.module';
-import { CommentModule } from './comment/comment.module';
-import { LikeModule } from './like/like.module';
-import { ViewModule } from './view/view.module';
-import { FollowModule } from './follow/follow.module';
 import { BoardArticleModule } from './board-article/board-article.module';
+import { CommentModule } from './comment/comment.module';
+import { FollowModule } from './follow/follow.module';
+import { LikeModule } from './like/like.module';
+import { MemberModule } from './member/member.module';
+import { NotificationModule } from './notification/notification.module';
+import { PropertyModule } from './property/property.module';
+import { ViewModule } from './view/view.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { BoardArticleModule } from './board-article/board-article.module';
     ViewModule,
     FollowModule,
     BoardArticleModule,
+    NotificationModule,
   ],
 })
 export class ComponentsModule {}
